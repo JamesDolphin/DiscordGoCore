9e2c16a57d6a6505ccc4ddbf963b13aadf948779
James Dolphin <j.dolphin2013@gmail.com>
2020-03-08 00:59:53 +1100

    Removed uneeded using statements

--- DiscordGo/Classes/GuildManager.cs
+++ DiscordGo/Classes/GuildManager.cs
@@ -1,7 +1,5 @@
 ﻿using Discord.WebSocket;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DiscordGo.Classes
 {
@@ -10,6 +8,7 @@ namespace DiscordGo.Classes
         public SocketGuild Guild { get; set; }
 
         public List<CsServer> Servers = new List<CsServer>();
+
         public GuildManager(SocketGuild guild)
         {
             Guild = guild;
--- DiscordGo/Constants/ChannelNames.cs
+++ DiscordGo/Constants/ChannelNames.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DiscordGo.Constants
+﻿namespace DiscordGo.Constants
 {
     public static class ChannelNames
     {
