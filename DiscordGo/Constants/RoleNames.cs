6d43b9911d947df97abf95b3a2b77222b71db90e
James Dolphin <j.dolphin2013@gmail.com>
2020-03-09 00:32:23 +1100

    added role names and helpers

---
+++ DiscordGo/Constants/RoleNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordGo.Constants
+{
+    public static class RoleNames
+    {
+        public const string Admin = "DiscordGo";
+        public const string Broadcast = "Broadcast";
+    }
+}
---
+++ DiscordGo/Utils/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace DiscordGo.Utils
+{
+    public static class Helpers
+    {
+        public static string GetPublicIPAddress()
+        {
+            string address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                address = stream.ReadToEnd();
+            }
+
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address[first..last];
+
+            return address;
+        }
+
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
