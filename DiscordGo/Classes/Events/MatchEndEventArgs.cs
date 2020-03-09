5b6b190453363ec856965489384d8de26b7e4500
James Dolphin <j.dolphin2013@gmail.com>
2020-03-09 22:01:34 +1100

    Added more event handler data classes

---
+++ DiscordGo/Classes/Events/MatchEndEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordGo.Classes.Events
+{
+    public class MatchEndEventArgs
+    {
+        public SocketGuild Guild { get; set; }
+        public string MapName { get; set; }
+
+        public string CTName { get; set; }
+
+        public string TName { get; set; }
+
+        public int CTScore { get; set; }
+
+        public int TScore { get; set; }
+        public int MatchId { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+        public int ServerId { get; set; }
+    }
+}
---
+++ DiscordGo/Classes/Events/OnGenericUpdateEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordGo.Classes.Events
+{
+    public class OnGenericUpdateEventArgs
+    {
+        public SocketGuild Guild { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+        public int ServerId { get; set; }
+    }
+}
